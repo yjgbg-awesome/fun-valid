a61ede2705d21af2bb405e8cc930ff2b574f3831
weicl <1239461052@qq.com>
2021-02-03 19:22:58 +0800

    允许根据错误值而改变的动态message 在校验出错误的时候记录错误值rejectValue 修整了包结构 继续添加对JSR380的支持(是个重体力活)

--- src/main/java/com/github/yjgbg/validation/core/Getter.java
+++ src/main/java/com/github/yjgbg/validation/core/Getter.java
@@ -9,12 +9,10 @@ import java.util.HashMap;
 import java.util.function.Function;
 
 /**
- * 表示一个javaBean中的get方法
- * 用途：希望对路径有类型安全的表达，而不是拼接字符串
- * <p>
- * 例如: Role role = new User().getRole();
- * Getter&lt;User,Role&gt; getter = User::getRole;
- * assert "role" == getter.propertyName();
+ * 表示一个javaBean中的get方法 用途：希望对路径有类型安全的表达，而不是拼接字符串
+ *
+ * <p>例如: Role role = new User().getRole(); Getter&lt;User,Role&gt; getter = User::getRole; assert
+ * "role" == getter.propertyName();
  *
  * @param <A> javaBean的类型
  * @param <B> get方法返回的类型
@@ -22,25 +20,27 @@ import java.util.function.Function;
 @FunctionalInterface
 public interface Getter<A, B> extends Function<A, B>, Serializable {
     HashMap<Class<?>, String> GETTER_NAME_CACHE = new HashMap<>();
-	@SneakyThrows
-	private String propertyName0() {
-		final var method = this.getClass().getDeclaredMethod("writeReplace");
-		method.setAccessible(Boolean.TRUE);
-		final var serializedLambda = (SerializedLambda) method.invoke(this);
-		final var getter = serializedLambda.getImplMethodName();
-		return Introspector.decapitalize(getter.replace("get", ""));
-	}
 
-	/**
-	 * 返回该get方法对应的属性的名称
-	 * @return 返回该get方法对应的属性的名称
-	 */
-	default String propertyName() {
-		final var clazz = getClass();
-		final var res0 = GETTER_NAME_CACHE.get(clazz);
-		if (res0 != null) return res0;
-		final var res = propertyName0();
-		GETTER_NAME_CACHE.put(clazz, res);
-		return res;
-	}
+    @SneakyThrows
+    private String propertyName0() {
+        final var method = this.getClass().getDeclaredMethod("writeReplace");
+        method.setAccessible(Boolean.TRUE);
+        final var serializedLambda = (SerializedLambda) method.invoke(this);
+        final var getter = serializedLambda.getImplMethodName();
+        return Introspector.decapitalize(getter.replace("get", ""));
+    }
+
+    /**
+     * 返回该get方法对应的属性的名称
+     *
+     * @return 返回该get方法对应的属性的名称
+     */
+    default String propertyName() {
+        final var clazz = getClass();
+        final var res0 = GETTER_NAME_CACHE.get(clazz);
+        if (res0 != null) return res0;
+        final var res = propertyName0();
+        GETTER_NAME_CACHE.put(clazz, res);
+        return res;
+    }
 }
