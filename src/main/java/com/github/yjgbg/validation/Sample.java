e8e5e77f9fc2824f08f0f667a34157c297b98320
weicl <1239461052@qq.com>
2021-02-04 19:29:51 +0800

    Error plus时丢失rejectValue的bug修复

--- src/main/java/com/github/yjgbg/validation/Sample.java
+++ src/main/java/com/github/yjgbg/validation/Sample.java
@@ -1,8 +1,8 @@
 package com.github.yjgbg.validation;
 
+import com.github.yjgbg.validation.core.BaseValidatorExt;
 import com.github.yjgbg.validation.core.Error;
 import com.github.yjgbg.validation.core.Validator;
-import com.github.yjgbg.validation.core.BaseValidatorExt;
 import com.github.yjgbg.validation.ext.JSR380Ext;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -17,13 +17,14 @@ import java.util.Objects;
 @ExtensionMethod({BaseValidatorExt.class, JSR380Ext.class})
 public class Sample {
   public static void main(String[] args) {
-    final var entity1 = new Entity1(null, 2L, true, Collections.emptyList());
-    final var validator =
-        Validator.<Entity1>failFast(true)
-            .assertTrue(Entity1::getField3)
-            .min(Entity1::getField2, 3L, "field2应该大于3")
-            .and(Entity1::getField1, Objects::nonNull, __ -> "field1不得为空");
-    final var errors = validator.apply(entity1);
+      final var entity1 = new Entity1(null, 2L, true, Collections.emptyList());
+      final var validator =
+              Validator.<Entity1>failFast(false)
+                      .assertTrue(Entity1::getField3)
+                      .min(Entity1::getField2, 3L, "field2应该大于3")
+                      .max(Entity1::getField2, 1L, "field2应该小于1")
+                      .and(Entity1::getField1, Objects::nonNull, __ -> "field1不得为null");
+      final var errors = validator.apply(entity1);
     final var requestBody = Error.wrapper("requestBody", errors);
     System.out.println(errors);
   }
--- src/main/java/com/github/yjgbg/validation/core/Error.java
+++ src/main/java/com/github/yjgbg/validation/core/Error.java
@@ -3,7 +3,6 @@ package com.github.yjgbg.validation.core;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +12,6 @@ import java.util.*;
 
 /** Error的构建，以及相关运算，包括Error的加法，以及Error与string的运算 */
 @Getter
-@ToString
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Error {
@@ -28,7 +26,7 @@ public class Error {
   }
 
   @Contract(pure = true)
-  public static Error message(@NotNull Object rejectValue, @NotNull final String message) {
+  public static Error message(@Nullable Object rejectValue, @NotNull final String message) {
       return new Error(rejectValue, Set.of(message), Map.of());
   }
 
@@ -56,14 +54,25 @@ public class Error {
   @NotNull
   @Contract(pure = true)
   public static Error plus(@Nullable final Error error1, @Nullable final Error error2) {
-    if (error1 == null) return plus(none(), error2);
-    if (error2 == null) return plus(error1, none());
-    if (error1==none()) return error2;
-    if (error2==none()) return error1;
-    final var messages = new HashSet<>(error1.getMessages());
-    error2.getMessages().stream().filter(x ->!messages.contains(x)).forEach(messages::add);
-    final var errors = new HashMap<>(error1.getErrors());
-    error2.getErrors().forEach((k, v) -> errors.put(k, plus(errors.get(k), v)));
-    return new Error(null,messages, errors);
+      if (error1 == null) return plus(none(), error2);
+      if (error2 == null) return plus(error1, none());
+      if (error1 == none()) return error2;
+      if (error2 == none()) return error1;
+      final var reject0 = error1.getRejectValue();
+      final var reject1 = error2.getRejectValue();
+      if (reject0 != reject1) throw new IllegalArgumentException(
+              String.format("不同数据对象的校验结果不可合并(%s,%s)", reject0, reject1));
+      final var messages = new HashSet<>(error1.getMessages());
+      error2.getMessages().stream().filter(x -> !messages.contains(x)).forEach(messages::add);
+      final var errors = new HashMap<>(error1.getErrors());
+      error2.getErrors().forEach((k, v) -> errors.put(k, plus(errors.get(k), v)));
+      return new Error(reject0, messages, errors);
   }
+
+    @Override
+    public String toString() {
+        if (rejectValue == null && messages.isEmpty()) return errors.toString();
+        if (errors.isEmpty()) return String.format("{'rejectValue':%s,'messages':%s}", rejectValue, messages);
+        return String.format("{'rejectValue':%s,'messages':%s,'errors':%s}", rejectValue, messages, errors);
+    }
 }
