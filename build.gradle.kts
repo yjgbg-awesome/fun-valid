1a27bb526bf2d8d81406922f547e50728d512dba
yjgbg <1239461052@qq.com>
2021-08-30 01:02:01 +0800

    simplify

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,7 @@ description = "fun-valid"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
+    mavenCentral()
 }
 
 dependencies {
@@ -26,17 +23,14 @@ java {
     withSourcesJar()
 }
 
-val mavenUsername:String by project
-val mavenPassword:String by project
-
 publishing {
     publications.create<MavenPublication>("this") {
         from(components["java"])
     }
     repositories.maven("https://oss.sonatype.org/content/repositories/snapshots") {
         credentials {
-            username = mavenUsername
-            password = mavenPassword
+            username = project.ext["mavenUsername"].toString()
+            password = project.ext["mavenPassword"].toString()
         }
     }
 }
